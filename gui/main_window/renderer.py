86c1a7d06f46729057e67fd9f4d48d50bace5e66
Shapovalov Vladislav <vshapovalov@ptsecurity.com>
2020-12-20 17:30:33 +0700

    refactor

--- gui/main_window/renderer.py
+++ gui/main_window/renderer.py
@@ -200,14 +200,3 @@ class Ui_MainWindow(object):
         self.pushButton_7.setText(_translate("MainWindow", "↺"))
         self.pushButton_9.setText(_translate("MainWindow", "-"))
         self.pushButton_10.setText(_translate("MainWindow", "Х"))
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
